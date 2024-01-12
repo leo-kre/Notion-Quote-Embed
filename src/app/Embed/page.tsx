53cb2c18375d117cafe2a3f28da47b81ba34c7b5
lfk <lf.kre@t-online.de>
2024-01-12 23:08:02 +0100

    adding default quote and adding loading text

--- src/app/Embed/page.tsx
+++ src/app/Embed/page.tsx
@@ -9,24 +9,42 @@ export default function Embed() {
       const textColor = "#" + searchParams.get("text");
       const accentColor = "#" + searchParams.get("accent");
 
-      const [quote, setQuote] = useState({ text: "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest", author: "~Walter Anderson" });
+      const defaultQuote = { text: "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest", author: "~Walter Anderson" };
+      const [quote, setQuote] = useState(defaultQuote);
+      const [loading, setLoading] = useState(true);
 
       useEffect(() => {
             fetch("https://type.fit/api/quotes")
-                  .then((response) => response.json())
+                  .then((response) => {
+                        if (!response.ok) {
+                              throw new Error("Network response was not ok");
+                        }
+                        return response.json();
+                  })
                   .then((data) => {
                         setQuote({ text: data[0].text, author: "~" + data[0].author.replace(", type.fit", "") });
+                        setLoading(false);
+                  })
+                  .catch((error) => {
+                        console.error("Fetch error: ", error);
+                        setLoading(false);
                   });
       }, []);
 
       return (
             <div className={"p-10 pl-5 py-5 min-h-screen flex flex-col justify-center"} style={{ background: `${bgColor}` }}>
-                  <h1 className={`text-xl md:text-3xl font-extrabold mb-2`} style={{ color: `${textColor}` }}>
-                        {quote.text}
-                  </h1>
-                  <h2 className={`text-lg font-semibold`} style={{ color: `${accentColor}` }}>
-                        {quote.author}
-                  </h2>
+                  {loading ? (
+                        <p>Loading...</p>
+                  ) : (
+                        <>
+                              <h1 className={`text-xl md:text-3xl font-extrabold mb-2`} style={{ color: `${textColor}` }}>
+                                    {quote.text}
+                              </h1>
+                              <h2 className={`text-lg font-semibold`} style={{ color: `${accentColor}` }}>
+                                    {quote.author}
+                              </h2>
+                        </>
+                  )}
             </div>
       );
 }
