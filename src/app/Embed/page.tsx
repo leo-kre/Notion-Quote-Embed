e058adf45f561ccf6347510c708c0aa508689725
lfk <lf.kre@t-online.de>
2024-01-12 22:32:17 +0100

    adding outline

--- src/app/Embed/page.tsx
+++ src/app/Embed/page.tsx
@@ -20,13 +20,15 @@ export default function Embed() {
       }, []);
 
       return (
-            <div className={"p-10 pl-5 py-5 min-h-screen flex flex-col justify-center"} style={{ background: `${bgColor}` }}>
-                  <h1 className={`text-3xl font-extrabold mb-2`} style={{ color: `${textColor}` }}>
-                        {quote.text}
-                  </h1>
-                  <h2 className={`text-lg font-semibold`} style={{ color: `${accentColor}` }}>
-                        {"~" + quote.author}
-                  </h2>
+            <div style={{ background: `${bgColor}` }}>
+                  <div className={"p-10 pl-5 py-5 min-h-screen flex flex-col justify-center border-2 border-[#252525] rounded-md"}>
+                        <h1 className={`text-3xl font-extrabold mb-2`} style={{ color: `${textColor}` }}>
+                              {quote.text}
+                        </h1>
+                        <h2 className={`text-lg font-semibold`} style={{ color: `${accentColor}` }}>
+                              {"~" + quote.author}
+                        </h2>
+                  </div>
             </div>
       );
 }
